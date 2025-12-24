05f7de4f22e0da6d5b0c7afab9e47f3c5a6ce41f
anamayasantii <anamayasanti18@gmail.com>
2025-12-24 11:50:33 +0800

    refactor: improve auth initialization in app.vue for better async handling

--- app.vue
+++ app.vue
@@ -5,15 +5,14 @@
     </div>
   </NuxtLayout>
 </template>
-<!-- <script setup>
+
+<script setup>
 import { useAuthStore } from '@/store/auth'
 
 const authStore = useAuthStore()
 
-await authStore.initAuth()
-
 // Init auth saat app start
-onMounted(() => {
-  authStore.initAuth()
+onMounted(async () => {
+  await authStore.initAuth()
 })
-</script> -->
+</script>
