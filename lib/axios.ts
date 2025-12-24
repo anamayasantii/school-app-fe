7f71dbfd85e9245ec7101ec162f170e574eaa7c0
anamayasantii <anamayasanti18@gmail.com>
2025-12-24 21:04:36 +0800

    refactor: set baseURL dynamically based on runtime configuration

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 
+const config = useRuntimeConfig();
+const appEnv = config.public.appEnv;
+
+const baseURL = appEnv === 'local' 
+  ? config.public.apiBaseUrlLocal 
+  : config.public.apiBaseUrl;
+
 const instance = axios.create({
+  baseURL: baseURL,
   validateStatus: (status) => {
     return status >= 200 && status < 300;
   },
